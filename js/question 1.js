03cbfada01c3f2f67935cf67b8e05644f6ba1f71
willyongaks <83573335+willyongaks@users.noreply.github.com>
2021-11-05 15:55:23 +0100

    Update question 1.js

--- js/question 1.js
+++ js/question 1.js
@@ -1,1 +1,13 @@
+//Convert the function below to an arrow function:
+
+//function getReminder(a,b) {
+  //  return a % b ;
+//}
+
+
+//answer 1:
+
+const  getReminder = (a,b) => (a % b);
+
+ console.log(getReminder(8, 9));
 
